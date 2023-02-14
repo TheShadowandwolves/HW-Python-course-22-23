8e074919b0daa2425da9e965efce50ae9b100679
Leonard Blam <blam@g.jct.ac.il>
2023-02-14 20:04:32 +0200

    fixed option 1 & 2

--- Project/Project.py
+++ Project/Project.py
@@ -1,27 +1,35 @@
 import matplotlib.pyplot as plt
 from colorama import Fore, Back, Style
+import numpy as np
+import numpy_financial as npf
+
+def FV(present_value, interest_rate, num_payments):
+    return (present_value * (pow((1+interest_rate), num_payments)-1) * (1+interest_rate)) / interest_rate
 
 def future_value_of_payments(interest_rate):
     try:
         print(Fore.GREEN)
         num_payments = int(input("Enter the number of payments: "))
         present_value = float(input("Enter the present value: "))
-        
         print(Style.RESET_ALL)
     except ValueError:
         print(Fore.RED + "Error: Please enter a valid number of payments and payment amount." + Style.RESET_ALL)
         future_value_of_payments(interest_rate)
    # print(Fore.BLUE + f'The future value of the payments is:{future_value}' + Style.RESET_ALL)
-    FV = []
-    last_future_value = present_value
-    for i in range(1, num_payments):
-        present_value = last_future_value
-        future_value = present_value * (1 + interest_rate /100)**num_payments
-        FV.append(future_value)
-        last_future_value = future_value
+    # FV = []
+    # last_future_value = present_value
+    # for i in range(1, num_payments):
+    #     present_value = last_future_value
+    #     future_value = present_value * (1 + interest_rate /100)**num_payments
+    #     FV.append(future_value)
+    #     last_future_value = future_value
     # Plotting the graph
-    x = list(range(1, num_payments))
-    y = FV
+    values = {}
+    for j in range(0, num_payments):
+        values[j] = FV(present_value, interest_rate, j)
+    x = list(range(0, num_payments))
+    y = list(values.values())
+        
     plt.plot(x, y)
     plt.xlabel("Payment Number")
     plt.ylabel("Future Value")
@@ -36,19 +44,22 @@ def payments_against_loan(interest_rate):
         print(Fore.GREEN)
         loan_amount = float(input("Enter the loan amount: "))
         num_payments = int(input("Enter the number of payments: "))
-        payment_amount = loan_amount * interest_rate * (1 + interest_rate)**num_payments / ((1 + interest_rate)**num_payments - 1)
+        # payment_amount = loan_amount * interest_rate * (1 + interest_rate)**num_payments / ((1 + interest_rate)**num_payments - 1)
         print(Style.RESET_ALL)
     except ValueError:
         print(Fore.RED + "Error: Please enter a valid loan amount and number of payments." + Style.RESET_ALL)
         payments_against_loan(interest_rate)
 
+    payment_amount = -1 * npf.pmt(interest_rate, num_payments, loan_amount)
     print(Fore.BLUE + f'The payment amount is: {payment_amount}' + Style.RESET_ALL)
-    
+    payment_left = FV(loan_amount, interest_rate, num_payments)
+    print(payment_left)
     payments = []
-    for i in range(1, num_payments + 1):
+    for i in range(0, num_payments):
         payments.append(payment_amount)
-    
-    plt.plot(range(1, num_payments + 1), payments)
+    x = [i for i in range(0, num_payments)]
+    y = payments
+    plt.plot(x, y)
     plt.xlabel("Duration of Loan (Months)")
     plt.ylabel("Payment Amount (Dollars)")
     plt.title("Payments Against Loan")
